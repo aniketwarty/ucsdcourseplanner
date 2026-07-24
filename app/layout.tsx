fab044c1e238fe9e2d23f75551333afda72e5f8f
aniketwarty <aniket@gomultiply.com>
2026-07-23 19:41:25 -0700

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Libre_Franklin, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const libreFranklin = Libre_Franklin({
   variable: "--font-libre-franklin",
@@ -30,7 +31,10 @@ export default function RootLayout({
       lang="en"
       className={`${libreFranklin.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
